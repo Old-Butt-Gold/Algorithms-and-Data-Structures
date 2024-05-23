4701fd7a20d80dcdc66763e8efe14b07e9f9f8d2
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2024-05-23 23:16:28 +0300

    updated dir Arrays

---
+++ AaDS/Arrays/ContainerWithMostWater.cs
@@ -0,0 +1,39 @@
+﻿namespace AaDS.Arrays;
+
+public static class ContainerWithMostWater
+{
+    /// <summary>
+    /// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+    /// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+    /// Return the maximum amount of water a container can store.
+    /// </summary>
+    /// <param name="height"></param>
+    /// <returns></returns>
+    public static int MaxArea(int[] height)
+    {
+        int max = 0;
+        int left = 0;
+        int right = height.Length - 1;
+        
+        while (left < right)
+        {
+            int width = right - left;
+            int h;
+
+            if (height[right] > height[left])
+            {
+                h = height[left];
+                left++;
+            }
+            else
+            {
+                h = height[right];
+                right--;
+            }
+
+            max = Math.Max(max, width * h);
+        }
+
+        return max;
+    }
+}
---
+++ AaDS/Arrays/TwoSum.cs
@@ -0,0 +1,63 @@
+﻿namespace AaDS.Arrays;
+
+public static class TwoSum
+{
+    /// <summary>
+    /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    /// You can return the answer in any order.
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public static int[] TwoSumI(int[] nums, int target)
+    {
+        Dictionary<int, int> dictionary = new();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int result = target - nums[i];
+
+            if (dictionary.TryGetValue(result, out var value))
+            {
+                return [value, i];
+            }
+
+            if (!dictionary.ContainsKey(nums[i]))
+            {
+                dictionary.Add(nums[i], i);
+            }
+        }
+
+        return null;
+    }
+    
+    /// <summary>
+    /// Input Array is sorted in non-decreasing order
+    /// </summary>
+    /// <param name="numbers"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public static int[] TwoSumII(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.Length - 1;
+
+        while (left < right)
+        {
+            var sum = numbers[left] + numbers[right];
+            if (sum == target) return [left, right];
+            else if (sum < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        
+        return null;
+    }
+    
+    
+}
