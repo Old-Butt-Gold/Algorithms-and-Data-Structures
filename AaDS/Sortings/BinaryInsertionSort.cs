c3684713f54b46483ee872a2738b73e20e45849c
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2024-07-06 00:51:57 +0300

    fixed BinaryInsertionSort

--- AaDS/Sortings/BinaryInsertionSort.cs
+++ AaDS/Sortings/BinaryInsertionSort.cs
@@ -4,34 +4,32 @@ namespace AaDS.Sortings;
 
 class BinaryInsertionSort<T> where T : IComparable<T>
 {
-    public static List<T> Sort(IEnumerable<T> collection, SortDirection sortDirection = SortDirection.Ascending)
+    public static void Sort(IList<T> collection, SortDirection sortDirection = SortDirection.Ascending)
     {
         CustomComparer<T> comparer = new CustomComparer<T>(sortDirection, Comparer<T>.Default);
-        var list = collection.ToList();
-        for (int i = 1; i < list.Count; i++)
+        for (int i = 1; i < collection.Count; i++)
         {
             int index = i - 1;
-            var item = list[i];
+            var item = collection[i];
 
             int location = BinarySearch(item, 0, index);
 
             for (; index >= location; index--)
             {
-                list[index + 1] = list[index];
+                collection[index + 1] = collection[index];
             }
 
-            list[index + 1] = item;
+            collection[index + 1] = item;
         }
-        return list;
 
         int BinarySearch(T item, int left, int right)
         {
             while (left <= right)
             {
                 int mid = (left + right) / 2;
-                if (EqualityComparer<T>.Default.Equals(item, list[mid]))
+                if (EqualityComparer<T>.Default.Equals(item, collection[mid]))
                     return mid + 1;
-                else if (comparer.Compare(item, list[mid]) > 0)
+                else if (comparer.Compare(item, collection[mid]) > 0)
                     left = mid + 1;
                 else
                     right = mid - 1;
