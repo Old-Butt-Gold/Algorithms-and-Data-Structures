8eaead2e1232e1a3d8b2b21aec7991bfa25ddebe
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2024-07-05 19:09:00 +0300

    fixed bubbleSort

--- AaDS/Sortings/BubbleSort.cs
+++ AaDS/Sortings/BubbleSort.cs
@@ -4,25 +4,26 @@ namespace AaDS.Sortings;
 
 static class BubbleSort<T> where T : IComparable<T>
 {
-    public static List<T> Sort(IEnumerable<T> collection, SortDirection sortDirection = SortDirection.Ascending)
+    public static void Sort(IList<T> collection, SortDirection sortDirection = SortDirection.Ascending)
     {
-        List<T> array = collection.ToList();
         var comparer = new CustomComparer<T>(sortDirection, Comparer<T>.Default);
         var swapped = true;
+        int counter = 0;
 
-        while (swapped)
+        do
         {
             swapped = false;
 
-            for (var i = 0; i < array.Count - 1; i++)
+            for (var i = 0; i < collection.Count - 1 - counter; i++)
             {
-                if (comparer.Compare(array[i], array[i + 1]) > 0)
+                if (comparer.Compare(collection[i], collection[i + 1]) > 0)
                 {
-                    (array[i], array[i + 1]) = (array[i + 1], array[i]);
+                    (collection[i], collection[i + 1]) = (collection[i + 1], collection[i]);
                     swapped = true;
                 }
             }
-        }
-        return array;
+
+            counter++;
+        } while (swapped);
     }
 }
